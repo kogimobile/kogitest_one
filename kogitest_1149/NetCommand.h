ab39d45f07c7ad5fe7c25d15927d7e3c27de1260
Omar Gomez <omar.gomez@gmail.com>
2011-04-30 23:15:57 -0500

    ftp

---
+++ kogitest_1149/NetCommand.h
@@ -0,0 +1,16 @@
+//
+//  NetCommand.h
+//  kogitest_1149
+//
+//  Created by Omar Gomez on 4/30/11.
+//  Copyright 2011 EAFIT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NetCommand : NSObject {
+    
+}
+
+@end
---
+++ kogitest_1149/NetCommand.m
@@ -0,0 +1,14 @@
+//
+//  NetCommand.m
+//  kogitest_1149
+//
+//  Created by Omar Gomez on 4/30/11.
+//  Copyright 2011 EAFIT. All rights reserved.
+//
+
+#import "NetCommand.h"
+
+
+@implementation NetCommand
+
+@end
